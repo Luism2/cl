574ddc68e29dbc1ce6a502cc19f879613b45de51
Luis Miguel Vazquez Juarez <luisjuares73@gmail.com>
2022-11-24 01:34:05 -0600

    V2

---
+++ lib/widgets/date_picker_title_widget.dart
@@ -0,0 +1,28 @@
+import 'package:register/res/colors.dart';
+import 'package:register/utills/constants.dart';
+import 'package:register/utills/extensions.dart';
+import 'package:flutter/material.dart';
+
+class DatePickerTitle extends StatelessWidget {
+  const DatePickerTitle({
+    required this.date,
+    super.key,
+  });
+
+  final DateTime date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(top: 16),
+        alignment: Alignment.centerLeft,
+        child: Text(
+          date.format(kMonthFormatWidthYear),
+          style: const TextStyle(
+            fontSize: 21,
+            color: violet,
+            fontWeight: FontWeight.w500,
+          ),
+        ));
+  }
+}
